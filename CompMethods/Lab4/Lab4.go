ed9861ccf051d4813782bc511c01580f9e4328c1
Kiiashchenko_Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-03-24 12:09:17 +0200

    Lab4-done

---
+++ CompMethods/Lab4/Lab4.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	const N = 10
+	years := [N]int{8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
+	y := [N]float64{68.27, 69.29, 70.44, 71.02, 71.15, 71.37, 71.38, 71.68, 71.98, 71.76}
+	var sumX, sumY, sumXY, sumXSquare, sumSquareX float64 = 0, 0, 0, 0, 0
+
+	for i := 0; i < N; i++ {
+		sumXY += float64(years[i]) * y[i]
+		sumX += float64(years[i])
+		sumY += y[i]
+		sumXSquare += float64(years[i]) * float64(years[i])
+	}
+	sumSquareX = sumX * sumX
+
+	var a = (N*sumXY - sumX*sumY) / (N*sumXSquare - sumSquareX)
+	var b = (sumY - a*sumX) / N
+	fmt.Printf("a: %v\n", a)
+	fmt.Printf("b: %v\n", b)
+}
